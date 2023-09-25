17cea22a6f8865ac46706d48837ad0191f06fb49
Islamsaeedfahmy <121707916+Islamsaeedfahmy@users.noreply.github.com>
2023-09-25 08:18:31 +0300

    first-commit

---
+++ index.js
@@ -0,0 +1,17 @@
+let newTitle = document.querySelector(".apply-btn");
+
+newTitle.addEventListener("click", myFunction);
+
+function myFunction() {
+  alert("Thanks for Applying in Spacex🤩");
+}
+
+
+// window.addEventListener("scroll", function () {
+//   const navbar = document.querySelector("nav");
+//   if (window.scrollY > 0) {
+//       navbar.classList.add("sticky");
+//   } else {
+//       navbar.classList.remove("sticky");
+//   }
+// });
